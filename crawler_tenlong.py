d1ff7e12a7dc468128462111af79e4351abf40f5
chusiang <chusiang.lai@gmail.com>
2017-02-14 11:54:23 +0800

    disable ssl warn

--- crawler_tenlong.py
+++ crawler_tenlong.py
@@ -4,14 +4,20 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import urllib3
+import logging
+
+'''
+disable ssl warn message.
+'''
+
+urllib3.disable_warnings()
+logging.captureWarnings(True)
 
 # send get request and get reposoe.
-# res = requests.get('https://www.tenlong.com.tw/items/7121275074')
-res = requests.get('https://www.tenlong.com.tw/items/9864760807?item_id=1021576')
+res = requests.get('https://www.tenlong.com.tw/items/7121275074')
 
 soup = BeautifulSoup(res.text, 'lxml')
-
-# print soup.prettify()
 
 # get book info.
 book_info = soup.find(id='item_info')
@@ -21,14 +27,13 @@ print book_info
 book_context = ''
 for context in soup.find_all('p'):
   book_context += str(context)
-# print book_context
 
+# remove the extra text.
 remove_order_element     = book_context.replace('<p>\n\t下單後立即進貨\n</p>', '')
-remove_shipment_element  = remove_order_element.replace('<p>\n\t立即進貨\n</p>', '')
+remove_shipment_element  = remove_order_element.replace('<p>\n\t立即出貨\n</p>', '')
 remove_copyright_element = remove_shipment_element.replace('<p>Copyright ® 2016 Tenlong Computer Book Co, Ltd. All rights reserved.</p>', '')
-remove_last2_p = remove_copyright_element.replace('<p>\n<a href="/faq">客服與FAQ</a> |\n\t\t<a href="/about">連絡我們</a> |\n\t\t<a href="/privacy">隱私權政策</a> |\n\t\t<a href="/terms">服務條款</a>\n</p>', '')
-remove_last3_p = remove_last2_p.replace('<p>天瓏提供<strong>超商代收！</strong></p>', '')
+remove_footer = remove_copyright_element.replace('<p>\n<a href="/faq">客服與FAQ</a> |\n\t\t<a href="/about">連絡我們</a> |\n\t\t<a href="/privacy">隱私權政策</a> |\n\t\t<a href="/terms">服務條款</a>\n</p>', '')
+book_context = remove_footer.replace('<p>天瓏提供<strong>超商代收！</strong></p>', '')
 
-book_context = remove_last3_p
 print book_context
 
